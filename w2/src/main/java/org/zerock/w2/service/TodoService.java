e78702ed614e3daa751c9b41572117d3cec8c810
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-03-11 16:51:17 +0900

    #12 - 프로젝트 생성과 복사
    기존 프로젝트로부터 복사

---
+++ w2/src/main/java/org/zerock/w2/service/TodoService.java
@@ -0,0 +1,61 @@
+package org.zerock.w2.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.zerock.w2.dao.TodoDAO;
+import org.zerock.w2.domain.TodoVO;
+import org.zerock.w2.dto.TodoDTO;
+import org.zerock.w2.util.MapperUtil;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Log4j2
+public enum TodoService {
+    INSTANCE;
+
+    private TodoDAO dao;
+    private ModelMapper modelMapper;
+
+    TodoService() {
+        dao = new TodoDAO();
+        modelMapper = MapperUtil.INSTANCE.get();
+    }
+
+    public void register(TodoDTO todoDTO) throws SQLException {
+        TodoVO todoVo = modelMapper.map(todoDTO, TodoVO.class);
+
+//        System.out.println("todoVo = " + todoVo);
+        log.info(todoVo);
+
+        dao.insert(todoVo); //int 를 반환하므로 이를 이용해서 예외 처리도 가능
+    }
+
+    public List<TodoDTO> listAll() throws SQLException {
+        List<TodoVO> voList = dao.selectAll();
+
+        log.info("voList.............................");
+        log.info(voList);
+
+        return voList.stream()
+                .map(vo -> modelMapper.map(vo, TodoDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    public TodoDTO get(Long tno) throws SQLException {
+        log.info("tno: " + tno);
+        return modelMapper.map(dao.selectOne(tno), TodoDTO.class);
+    }
+
+    public void remove(Long tno) throws SQLException {
+        log.info("tno: " + tno);
+        dao.deleteOne(tno);
+    }
+
+    public void modify(TodoDTO todoDTO) throws SQLException {
+        log.info("todoDTO: " + todoDTO);
+
+        dao.updateOne(modelMapper.map(todoDTO, TodoVO.class));
+    }
+}
